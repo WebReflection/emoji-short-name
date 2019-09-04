4fd5e2c8001652542411eabd9d7e4e54a48646b6
Andrea Giammarchi <andrea.giammarchi@gmail.com>
2019-09-04 23:14:09 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# emoji-short-name
+
+An `{emoji: 'Short Name'}` dictionary directly from https://unicode.org/
+
+```js
+import emojiShortName from 'emoji-short-name';
+// or
+const emojiShortName = require('emoji-short-name');
+
+console.log(emojiShortName['🎉']);
+"party popper"
+```
---
+++ build.js
@@ -0,0 +1,40 @@
+const fs = require('fs');
+const {app, BrowserWindow} = require('electron');
+
+app.on('ready', () => {
+  const win = new BrowserWindow({
+    width: 1280,
+    height: 720,
+    images: false,
+    webgl: false
+  });
+  win.loadURL('https://unicode.org/emoji/charts/full-emoji-list.html');
+  console.log('please wait');
+  win.webContents.on('did-finish-load', () => {
+    win.webContents.executeJavaScript(
+      `(() => {
+        const emoji = {};
+        for (const code of document.querySelectorAll('td.code')) {
+          const text = code.textContent.trim();
+          const unicode = text.replace(/U\\+/g, '0x').replace(/[\\s\\n\\r\\f]+/g, ', ');
+          const key = eval("String.fromCodePoint(" + unicode + ")");
+          emoji[key] = code.parentElement.lastElementChild.textContent.trim();
+        }
+        return emoji;
+      })()`,
+      emoji => {
+        fs.writeFile(
+          'index.js',
+          `var emojiShortName = ${JSON.stringify(emoji, null, '  ')};\n`,
+          err => {
+            if (err) {
+              console.error(err);
+              process.exit(1);
+            }
+            win.close();
+          }
+        );
+      }
+    );
+  });
+});
